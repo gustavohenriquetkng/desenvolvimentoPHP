3808f9935c7a90eba191daa16c71112831697fad
KingTavin <gustavohrn8@gmail.com>
2026-02-26 18:34:58 -0300

    formatação de numeros

--- Helpers.php
+++ Helpers.php
@@ -1,31 +1,65 @@
 <?php
-function saudacao (): string{
 
-    $hora = 9;
+function formatarNumero(float $numero): string {
+    return number_format($numero, 2, ',', '.');
+}
+
+
+
+
+/**
+ * Summary of formatarValor
+ * @param float $valor
+ * @return string
+ */
+
+function formatarValor(float $valor): string {
+    return number_format(($valor?:10), 2, ',', '.');
     
 
-    if ($hora >= 0 && $hora <= 5){
+}
 
-        $saudacao = 'Boa Madrugada';
+/**
+ * Summary of saudacao
+ * @return string saudação apropriada com base na hora atual do dia
+ */
+function saudacao (): string{
+    date_default_timezone_set('America/Sao_Paulo'); // fuso do Brasil
 
-    }elseif ($hora >= 6 && $hora <= 11){
+    $hora = date('H'); // obtém a hora atual no formato de 24 horas
+    
+
+    if ($hora >= 5 && $hora <= 12){
+
         $saudacao = 'Bom Dia';
-    }else{
-            $saudacao = 'Boa Tarde';
+
+    }elseif ($hora >= 13 && $hora <= 18){
+        $saudacao = 'Boa Tarde';
+        
+    }elseif ($hora >= 19 && $hora <= 23){
+            $saudacao = 'Boa Noite';}
+    else{
+            $saudacao = 'Boa madrugada';
     }
     return $saudacao;
 
 }
 
-
+/**
+ * Summary of resumirTexto
+ * @param string $texto texto a ser resumido
+ * @param int $limite limite de caracteres para o resumo
+ * @param string $continuacao texto a ser adicionado ao final do resumo, caso o texto original seja maior que o limite
+ * @return string resumo do texto original, com a continuação caso necessário
+ */
 function resumirTexto( string $texto, int $limite, string $continuacao = '...'): string{
 
-    $textoLimpo = trim(strip_tags($texto));
+    $textoLimpo = trim(strip_tags($texto)); // remove tags HTML e espaços em branco do início e do fim do texto
     if (mb_strlen($textoLimpo) <= $limite){
         return $textoLimpo;
     }
 
-   $resumirTexto = mb_substr($textoLimpo, 0, mb_strrpos(mb_substr($textoLimpo, 0, $limite), ''));
+   $resumirTexto = mb_substr($textoLimpo, 0, mb_strrpos(mb_substr($textoLimpo, 0, $limite), ' ')); // obtém uma substring do texto limpo, começando do início e terminando no último espaço antes do limite de caracteres
     
     return $resumirTexto . $continuacao;
 }
--- index.php
+++ index.php
@@ -5,11 +5,23 @@ Ele é responsável por carregar as dependências e iniciar a aplicação.*/
 require_once './Sistema/configuracao.php';
 include_once 'Helpers.php';
 
-
-$texto = '<h1>Este é um texto longo que será</h1> <p>resumido para demonstração</p> do funcionamento da função resumirTexto.';
-
+echo formatarValor(0);  /*Chama a função formatarValor, passando o valor 123456.789 como argumento, e exibe o resultado formatado. */;
+$Valor = '333';  //Variável $Valor recebe o valor '333'. 
+echo '<br>';
+$texto = '<h1>Este é um texto longo que será</h1> <p>resumido para demonstração</p> do funcionamento da função resumirTexto.';  /* Variável $texto recebe uma string HTML que contém um texto longo. */
+echo '<br>';
 echo saudacao();
 echo '<br>';
-
 echo resumirTexto($texto, 500, '...');
-
+echo '<br>';
+echo ($Valor ?: 'Valor não definido');  /*Função de operador ternário para verificar se a variável $Valor
+ tem um valor definido. Se tiver, exibe o valor, caso contrário, exibe a mensagem "Valor não definido". */
+Echo '<br>';
+$numero1 = 0;  //Variável $numero recebe o valor 0.
+$numero2 = 10; //Variável $numero recebe o valor 10.
+$numero3 = 15; //Variável $numero recebe o valor 15.
+echo formatarNumero($numero1);  /*Chama a função formatarValor, passando o valor da variável $numero como argumento, e exibe o resultado formatado. */
+echo '<br>';
+echo formatarNumero($numero2);  /*Chama a função formatarValor, passando o valor da variável $numero como argumento, e exibe o resultado formatado. */
+echo '<br>';
+echo formatarNumero($numero3);  /*Chama a função formatarValor, passando o valor da variável $numero como argumento, e exibe o resultado formatado. */
